67a4ee3c7d1b3ab0a0711cb23a932f5506f8fb75
PhengVisal <visal.pheng@student.niptict.edu.kh>
2021-01-12 01:05:51 +0700

    add ckeditor to edit products

--- resources/views/backend/products/productsedit.blade.php
+++ resources/views/backend/products/productsedit.blade.php
@@ -37,7 +37,7 @@
                 
                 <div class="form-group">
                     <label for="description">Description</label>
-                    <textarea rows="5" class="form-control" id="pdesc" name="description" value="{{$products->description}}"></textarea>
+                    <textarea id="my-editor" name="description" class="form-control" id="pdesc"></textarea>
                 </div>
                 
                 <div class="form-group">
@@ -67,4 +67,16 @@
 
 @section('js')
     <script> console.log('Hi!'); </script>
+    <script src="//cdn.ckeditor.com/4.6.2/standard/ckeditor.js"></script>
+    <script>
+      var options = {
+        filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+        filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
+        filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+        filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
+      };
+    </script>
+    <script>
+        CKEDITOR.replace('my-editor', options);
+        </script>
 @stop
